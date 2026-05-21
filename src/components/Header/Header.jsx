88ca913723e0544d97902b4b586724d63e799966
Guillaume Duval <guillaume.duval-dev@orange.fr>
2026-05-21 16:36:52 +0200

    Intégration header et navigation via un composant Outlet

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router";
+import redLogo from "../../assets/images/LOGO-red.svg";
+import classes from "./header.module.scss";
+
+export default function Header() {
+  return (
+    <header className={classes.header}>
+      <img
+        src={redLogo}
+        alt="logo rouge de Kasa"
+        width="210"
+        height="68"
+        className={classes.header__logo}
+      />
+      <nav className={classes.header__navigation}>
+        <NavLink
+          to="/"
+          end=""
+          className={({ isActive }) => (isActive ? classes.active : "")}
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? classes.active : "")}
+        >
+          À propos
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from "./components/Layout/Layout.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Housing from "./pages/Housing/Housing.jsx";
 import About from "./pages/About/About.jsx";
 import Error from "./pages/Error/Error.jsx";
+import "./sass/global.scss"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/housing" element={<Housing />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/*" element={<Error />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/housing" element={<Housing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/*" element={<Error />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
