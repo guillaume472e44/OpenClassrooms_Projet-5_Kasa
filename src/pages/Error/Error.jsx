234dfe9c2d216f1afaf59185c3caf6621bf66f4f
Guillaume Duval <guillaume.duval-dev@orange.fr>
2026-05-25 12:20:31 +0200

    Intégration page 404

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router";
+import classes from "./error.module.scss";
+
 export default function Error() {
   return (
-    <div>Error</div>
-  )
+    <section className={classes.error}>
+      <h1>404</h1>
+      <p>
+        <span>Oups! La page que</span> <span>vous demandez n'existe pas.</span>
+      </p>
+      <Link to="/">Retourner sur la page d’accueil</Link>
+    </section>
+  );
 }
